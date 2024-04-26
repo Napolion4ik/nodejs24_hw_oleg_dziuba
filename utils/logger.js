21b24edbbbf0274605a996d987f2e37caacba821
evhenious <evhenious@gmail.com>
2024-04-26 09:21:32 +0300

    all content moved to root folder

---
+++ utils/logger.js
@@ -0,0 +1,37 @@
+const colors = require("colors");
+const config = require("config");
+
+function logger(identifier) {
+	config.get("colorsEnabled") ? null : colors.disable();
+
+	const info = (...text) => {
+		if (config.get("logLevel") === "info") {
+			console.log(`-------------------------------\n`.rainbow);
+			console.log(identifier.green.bgCyan, ": ", text.join(" | "));
+			console.log("\n-------------------------------".rainbow);
+		}
+	};
+
+	const warn = (...text) => {
+		if (
+			config.get("logLevel") === "info" ||
+			config.get("logLevel") === "warn"
+		) {
+			console.warn(`---------------------------------\n`.rainbow);
+			console.warn(identifier.bgYellow.cyan, ": ", text.join(" | "));
+			console.warn("\n-------------------------------".rainbow);
+		}
+	};
+	const error = (...text) => {
+		console.error(`-------------------------------\n`.brightRed);
+		console.error(identifier.brightRed, ": ", text.join(" | "));
+		console.error(`\n-------------------------------`.brightRed);
+	};
+	return {
+		info,
+		warn,
+		error,
+	};
+}
+
+module.exports = logger;
