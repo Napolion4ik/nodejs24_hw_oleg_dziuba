47ef7689d41edb43703ed86f9480b88e07c3dfb7
Napolion4ik <dziubaoleg20@gmail.com>
2024-05-06 01:27:05 +0300

    Done homework_4

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,42 @@
+const fs = require("fs");
 const colors = require("colors");
 const config = require("config");
+const path = require("path");
+
+const logsDir = path.resolve(__dirname, "../logs");
+
+function writeLogsToFile(writeStream, message) {
+	const date = new Date().toISOString();
+	writeStream.write(`${message}: ${date} \n`);
+}
+
+let initialized = false;
+let writeableStreamInfo, writeableStreamError;
 
 function logger(identifier) {
 	if (!config.get("colorsEnabled")) {
 		colors.disable();
 	}
 
+	if (!initialized) {
+		if (!fs.existsSync(logsDir)) {
+			fs.mkdir(logsDir, (err) => {
+				console.error(err);
+			});
+		}
+		writeableStreamInfo = fs.createWriteStream(
+			path.join(logsDir, "info.log"),
+			{ flags: "a" }
+		);
+		writeableStreamError = fs.createWriteStream(
+			path.join(logsDir, "errors.log"),
+			{ flags: "a" }
+		);
+		initialized = true;
+	}
+
 	const info = (...text) => {
+		writeLogsToFile(writeableStreamInfo, "Info logger");
 		if (config.get("logLevel") === "info") {
 			console.log(`-------------------------------\n`.rainbow);
 			console.log(identifier.green.bgCyan, ": ", text.join(" | "));
@@ -15,6 +45,7 @@ function logger(identifier) {
 	};
 
 	const warn = (...text) => {
+		writeLogsToFile(writeableStreamError, "Warn logger");
 		if (config.get("logLevel") !== "error") {
 			console.warn(`---------------------------------\n`.rainbow);
 			console.warn(identifier.bgYellow.cyan, ": ", text.join(" | "));
@@ -22,6 +53,7 @@ function logger(identifier) {
 		}
 	};
 	const error = (...text) => {
+		writeLogsToFile(writeableStreamError, "Error logger");
 		console.error(`-------------------------------\n`.brightRed);
 		console.error(identifier.brightRed, ": ", text.join(" | "));
 		console.error(`\n-------------------------------`.brightRed);
@@ -32,5 +64,10 @@ function logger(identifier) {
 		error,
 	};
 }
+
+process.on("beforeExit", () => {
+	writeableStreamInfo.end();
+	writeableStreamError.end();
+});
 
 module.exports = logger;
